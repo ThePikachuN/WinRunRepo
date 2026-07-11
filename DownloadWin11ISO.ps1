2eb999c0683b36890c0937563f92c3a2b7d9112d
ThePikachuN <mustafafaran8@outlook.com>
2026-07-11 13:57:30 +0100

    Update download message for Windows 11 ISO script

--- DownloadWin11ISO.ps1
+++ DownloadWin11ISO.ps1
@@ -4,6 +4,6 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/pbatard/Fido/master/Fi
 
 # 2. Execute Fido to grab the URL and start the download
 # This automatically fetches the latest stable Windows 11 x64 multi-edition in your system's language
-Write-Host "Querying Microsoft servers for the latest Windows 11 ISO link..." -ForegroundColor Cyan
+Write-Host "Downloading Windows 11 ISO... This may take 10 minutes..." -ForegroundColor Cyan
 
 & "$env:TEMP\Fido.ps1" -Win 11 -Arch x64
